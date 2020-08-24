767ac4db76026f4272b5f5f1c573e156a93873d6
Galang Cahyo Murdoko <galangcahyomurdoko@gmail.com>
2020-08-24 15:55:32 +0700

    done

--- app/src/main/java/com/example/bank_reboot/domain/user/fragment/UserDetailFragment.kt
+++ app/src/main/java/com/example/bank_reboot/domain/user/fragment/UserDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.bank_reboot.R
 import com.example.bank_reboot.domain.user.view_model.UserViewModel
@@ -16,7 +17,7 @@ import kotlinx.android.synthetic.main.fragment_user_detail.*
 class UserDetailFragment : Fragment() {
 
     private val userViewModel by activityViewModels<UserViewModel>()
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,11 +37,12 @@ class UserDetailFragment : Fragment() {
         userViewModel.user.observe(viewLifecycleOwner, Observer {
             balance_user_datail.text = it.balance.toString()
         })
+        navController = Navigation.findNavController(view)
         history_button.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_userDetailFragment_to_userTransactionHistoryFragment)
         }
         transfer_to_button.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_userTansferAmountFragment_to_userTransferToFragment)
+            Navigation.findNavController(view).navigate(R.id.action_userDetailFragment_to_userTransferToFragment)
         }
     }
 }
